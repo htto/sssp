a70c3b67ef72552eb270bb92ad47b970ec65fe48
htto <htto@users.noreply.github.com>
2015-10-08 16:06:08 +0200

    Enable log support

--- src/sssp.c
+++ src/sssp.c
@@ -102,12 +102,12 @@ static void *findHook(const char *mod, const char *name)
 	void *m = mod ? dlopen(mod, RTLD_NOW | RTLD_GLOBAL) : RTLD_NEXT;
 
 	if (mod && !m)
-		fprintf(stderr, "Unable to query module %s!\n", mod);
+		log(LOG_ERROR, "Unable to query module %s!\n", mod);
 
 	h = m ? g_realDlsym(m, name) : NULL;
 
 	if (!h)
-		fprintf(stderr, "Unable to hook %s!\n", name);
+		log(LOG_ERROR, "Unable to hook %s!\n", name);
 
 	return h;
 }
@@ -151,7 +151,7 @@ static Bool findDlSym(void)
 
 	dlclose(mm);
 #else
-	fprintf(stderr, "No dlsym hooking possible. Expect issues.\n");
+	log(LOG_ERROR, "No dlsym hooking possible. Expect issues.\n");
 	g_realDlsym = (hookPPFunc)dlsym;
 #endif
 	return g_realDlsym != NULL;
@@ -166,7 +166,7 @@ __attribute__((constructor)) static void init(void)
 	if (program_invocation_name)
 		program_invocation_short_name = basename(program_invocation_name);
 #endif
-	fprintf(stderr, "sssp_xy.so loaded into program '%s' (%s).\n",
+	log(LOG_NOTICE, "sssp_xy.so loaded into program '%s' (%s).\n",
 			program_invocation_short_name, program_invocation_name);
 
 	/* TODO blacklist steam* executables */
@@ -174,16 +174,16 @@ __attribute__((constructor)) static void init(void)
 
 	if (ssspRunning)
 	{
-		fprintf(stderr, "SSSP already loaded! Check your LD_PRELOAD.\n");
+		log(LOG_ERROR, "SSSP already loaded! Check your LD_PRELOAD.\n");
 		/* TODO don't fail? */
-		exit(1);
+		return;
 	}
 
 	ssspRunning = True;
 
 	if (!findDlSym())
 	{
-		fprintf(stderr, "ERROR: Unable to set up dlsym hook. Won't work this way. "
+		log(LOG_ERROR, "Unable to set up dlsym hook. Won't work this way. "
 				"Please disable this module from being LD_PRELOAD'ed.\n");
 		return;
 	}
@@ -195,7 +195,7 @@ __attribute__((constructor)) static void init(void)
 
 	if (!(g_realXEventsQueued && g_realXLookupString && g_realXOpenDisplay && g_realXPending))
 	{
-		fprintf(stderr, "ERROR: Unable to set up X11 hooks. Won't work this way. "
+		log(LOG_ERROR, "Unable to set up X11 hooks. Won't work this way. "
 				"Please disable this module from being LD_PRELOAD'ed.\n");
 		return;
 	}
@@ -207,25 +207,25 @@ __attribute__((constructor)) static void init(void)
 	sevp.sigev_notify_attributes = NULL;
 	int rc = timer_create(CLOCK_MONOTONIC, &sevp, &g_userFbTimer);
 	if (rc)
-		perror("timer_create(g_userFbTimer)");
+		log(LOG_ERROR, "timer_create(g_userFbTimer): %s\n", strerror(errno));
 
 	sevp.sigev_notify_function = screenshotTimerHandler;
 	sevp.sigev_notify_attributes = NULL;
 	timer_create(CLOCK_MONOTONIC, &sevp, &g_screenshotTimer);
 	if (rc)
-		perror("timer_create(g_screenshotTimer)");
+		log(LOG_ERROR, "timer_create(g_screenshotTimer): %s\n", strerror(errno));
 
 	/* Init X11 thread support */
 	XInitThreads();
 
-	fprintf(stderr, "sssp_xy.so initialized.\n");
+	log(LOG_NOTICE, "sssp_xy.so initialized.\n");
 }
 
 /* Finalization */
 __attribute__((destructor)) static void deinit(void)
 {
 	/* TODO */
-	fprintf(stderr, "sssp_xy.so being unloaded from program '%s' (%s).\n",
+	log(LOG_NOTICE, "sssp_xy.so being unloaded from program '%s' (%s).\n",
 			program_invocation_short_name, program_invocation_name);
 
 	if (g_userFbTimer)
@@ -266,7 +266,7 @@ static void *captureScreenShot(Display *dpy, Window *win, int *w, int *h)
 
 	if (XGetWindowAttributes(dpy, *win, &attrs) == 0)
 	{
-		fprintf(stderr, "failed to acquire window attributes!");
+		log(LOG_ERROR, "failed to acquire window attributes!");
 		return NULL;
 	}
 
@@ -284,9 +284,7 @@ static void *captureScreenShot(Display *dpy, Window *win, int *w, int *h)
 			break;
 		}
 
-#if DEBUG > 2
-		fprintf(stderr, "XTranslateCoordinates: %d/%d %d/%d 0x%lx %d/%d\n", attrs.x, attrs.y, attrs.width, attrs.height, c, dx, dy);
-#endif
+		log(LOG_INFO, "XTranslateCoordinates: %d/%d %d/%d 0x%lx %d/%d\n", attrs.x, attrs.y, attrs.width, attrs.height, c, dx, dy);
 		p = c;
 	}
 
@@ -295,13 +293,11 @@ static void *captureScreenShot(Display *dpy, Window *win, int *w, int *h)
 	/* TODO switch to XRenderCreatePicture */
 	if ((image = XGetImage(dpy, *win, 0, 0, attrs.width, attrs.height, AllPlanes, ZPixmap)) == NULL)
 	{
-		fprintf(stderr, "failed to acquire window screenshot!");
+		log(LOG_ERROR, "failed to acquire window screenshot!");
 		return NULL;
 	}
 
-#if DEBUG > 0
-	fprintf(stderr, "Grabbed image of window 0x%lx (size %dx%d, depth %d).\n", *win, image->width, image->height, image->depth);
-#endif
+	log(LOG_NOTICE, "Grabbed image of window 0x%lx (size %dx%d, depth %d).\n", *win, image->width, image->height, image->depth);
 
 	/* Convert to plain RGB as required by steam. */
 	*h = attrs.height;
@@ -334,13 +330,11 @@ static void handleScreenShot(Display *dpy, Window win)
 	tval.it_interval.tv_sec = 0;
 	tval.it_interval.tv_nsec = 0;
 
-#if DEBUG > 0
-	fprintf(stderr, "%s()\n", __FUNCTION__);
-#endif
+	log(LOG_NOTICE, "%s()\n", __FUNCTION__);
 
 	int rc = timer_settime(g_screenshotTimer, 0, &tval, NULL);
 	if (rc)
-		perror("timer_settime(g_screenshotTimer)");
+		log(LOG_ERROR, "timer_settime(g_screenshotTimer): %s\n", strerror(errno));
 }
 
 static void doScreenShot(Display *dpy, Window win)
@@ -350,7 +344,7 @@ static void doScreenShot(Display *dpy, Window win)
 	XWindowAttributes attrs;
 	union sigval unused;
 
-	fprintf(stderr, "doScreenShot(%p, 0x%lx)\n", dpy, win);
+	log(LOG_NOTICE, "doScreenShot(%p, 0x%lx)\n", dpy, win);
 
 	/* Hide feedback window */
 	userFbTimerHandler(unused);
@@ -381,7 +375,7 @@ static void doScreenShot(Display *dpy, Window win)
 			/* And destroy on size change */
 			if (attrs.width != g_oldFbAttrs.width || attrs.height != g_oldFbAttrs.height)
 			{
-				fprintf(stderr, "XDestroyWindow(g_userFbWin)\n");
+				log(LOG_NOTICE, "XDestroyWindow(g_userFbWin)\n");
 				XDestroyWindow(dpy, g_userFbWin);
 				g_userFbWin = 0;
 			}
@@ -429,19 +423,19 @@ static void doScreenShot(Display *dpy, Window win)
 		tval.it_interval.tv_nsec = 0;
 		int rc = timer_settime(g_userFbTimer, 0, &tval, NULL);
 		if (rc)
-			perror("timer_settime(g_userFbTimer)");
+			log(LOG_ERROR, "timer_settime(g_userFbTimer): %s\n", strerror(errno));
 	}
 
 	/* Issue the RGB image directly to steam. */
 	if (g_steamInitialized)
 	{
 		if (!g_steamIScreenshot->vtab->WriteScreenshot(g_steamIScreenshot, image, 3 * w * h, w, h))
-			fprintf(stderr, "Failed to issue screenshot to steam.\n");
+			log(LOG_ERROR, "Failed to issue screenshot to steam.\n");
 	}
 	else
 	{
 		// TODO
-		fprintf(stderr, "Steam not initialized, no screenshot saved.\n");
+		log(LOG_ERROR, "Steam not initialized, no screenshot saved.\n");
 #if 0
 		/* Hand made fallback disabled in favour of Steam's screenshot writer. */
 		static uint32_t count = 0;
@@ -458,9 +452,7 @@ static void doScreenShot(Display *dpy, Window win)
 		snprintf(path, sizeof(path), "%s/userdata/%d/760/remote/%d/screenshots/%s_%05d.jpg",
 				SteamAPI_GetSteamInstallPath(), g_steamUserID.asComponent.accountID,
 				g_steamAppID, date, count);
-#if DEBUG > 2
-		fprintf(stderr, "screenshot file: '%s'\n", path);
-#endif
+		log(LOG_INFO, "screenshot file: '%s'\n", path);
 
 		/* TODO mkdir -p */
 #endif
@@ -483,20 +475,20 @@ static void doStatsUpdate()
 		while (scount && i++ < scount - 1)
 		{
 			const char *sname = g_steamIUserStats->vtab->GetStatName(g_steamIUserStats, g_steamAppID, i);
-			fprintf(stderr, "stats[%d].sname: %s\n", i, sname);
+			log(LOG_INFO, "stats[%d].sname: %s\n", i, sname);
 			Bool sachieved = False;
 			uint32_t tstamp = 0;
 			if (g_steamIUserStats->vtab->GetAchievementAndUnlockTime(g_steamIUserStats, g_steamAppID, sname, &sachieved, &tstamp))
 			{
-				fprintf(stderr, "stats[%d].achieved: %d\n", i, sachieved);
+				log(LOG_INFO, "stats[%d].achieved: %d\n", i, sachieved);
 				if (sachieved)
-					fprintf(stderr, "stats[%d].tsamp: 0x%x\n", i, tstamp);
+					log(LOG_INFO, "stats[%d].tsamp: 0x%x\n", i, tstamp);
 			}
 
 			int32_t sdata = -1;
 			if (g_steamIUserStats->vtab->GetStat(g_steamIUserStats, g_steamAppID, sname, &sdata))
 			{
-				fprintf(stderr, "stats[%d].data: 0x%x\n", i, sdata);
+				log(LOG_INFO, "stats[%d].data: 0x%x\n", i, sdata);
 			}
 		}
 	}
@@ -508,23 +500,23 @@ static void doStatsUpdate()
 		while (scount && i++ < scount - 1)
 		{
 			const char *sname = g_steamIUserStats->vtab->GetAchievementName(g_steamIUserStats, i);
-			fprintf(stderr, "stats[%d].sname: %s\n", i, sname);
+			log(LOG_INFO, "stats[%d].sname: %s\n", i, sname);
 			Bool sachieved = False;
 			uint32_t tstamp = 0;
 			if (g_steamIUserStats->vtab->GetAchievementAndUnlockTime(g_steamIUserStats, sname, &sachieved, &tstamp))
 			{
-				fprintf(stderr, "stats[%d].achieved: %d\n", i, sachieved);
+				log(LOG_INFO, "stats[%d].achieved: %d\n", i, sachieved);
 				if (sachieved)
-					fprintf(stderr, "stats[%d].tsamp: 0x%x\n", i, tstamp);
+					log(LOG_INFO, "stats[%d].tsamp: 0x%x\n", i, tstamp);
 			}
 
-			fprintf(stderr, "LALA: %s\n", g_steamIUserStats->vtab->GetAchievementDisplayAttribute(g_steamIUserStats, sname, "name"));
-			fprintf(stderr, "LALA: %s\n", g_steamIUserStats->vtab->GetAchievementDisplayAttribute(g_steamIUserStats, sname, "desc"));
-			fprintf(stderr, "LALA: %s\n", g_steamIUserStats->vtab->GetAchievementDisplayAttribute(g_steamIUserStats, sname, "statvalue"));
+			log(LOG_INFO, "NAME: %s\n", g_steamIUserStats->vtab->GetAchievementDisplayAttribute(g_steamIUserStats, sname, "name"));
+			log(LOG_INFO, "DESC: %s\n", g_steamIUserStats->vtab->GetAchievementDisplayAttribute(g_steamIUserStats, sname, "desc"));
+			log(LOG_INFO, "SVAL: %s\n", g_steamIUserStats->vtab->GetAchievementDisplayAttribute(g_steamIUserStats, sname, "statvalue"));
 			int32_t sdata = -1;
 			if (g_steamIUserStats->vtab->GetStat(g_steamIUserStats, sname, &sdata))
 			{
-				fprintf(stderr, "stats[%d].data: %x\n", i, sdata);
+				log(LOG_INFO, "stats[%d].data: %x\n", i, sdata);
 			}
 		}
 	}
@@ -538,25 +530,17 @@ static Bool filter(Display *dpy UNUSED, XEvent *event, XPointer arg UNUSED)
 	Bool rc = False;
 	static Time t = 0;
 
-#if DEBUG > 4
-	fprintf(stderr, "filter()\n");
-#endif
 	if (event->type == KeyPress /*|| event->type == KeyRelease*/)
 	{
-#if DEBUG > 4
-		fprintf(stderr, "key press/release\n");
-#endif
+		log(LOG_INFO, "key press/release\n");
+
 		ke = (XKeyEvent *)event;
 		if (!ke->send_event && !(ke->state & 0xFF/* kbd modifiers */))
 		{
-#if DEBUG > 3
-			fprintf(stderr, "got keycode: 0x%x\n", ke->keycode);
-#endif
+			log(LOG_INFO, "got keycode: 0x%x\n", ke->keycode);
 			if (ke->keycode == g_xKeyCodeF11)
 			{
-#if DEBUG > 2
-				fprintf(stderr, "Stats key recognized\n");
-#endif
+				log(LOG_NOTICE, "Stats key recognized\n");
 				rc = True;
 				/* TODO delay/call through thread */
 				doStatsUpdate();
@@ -568,14 +552,10 @@ static Bool filter(Display *dpy UNUSED, XEvent *event, XPointer arg UNUSED)
 				{
 					t = ke->time;
 					rc = True;
-#if DEBUG > 2
-					fprintf(stderr, "Screenshot key recognized\n");
-#endif
+					log(LOG_NOTICE, "Screenshot key recognized\n");
 				}
-#if DEBUG > 1
 				else
-					fprintf(stderr, "Screenshot key skipped due to flooding (<50ms)\n");
-#endif
+					log(LOG_WARN, "Screenshot key skipped due to flooding (<50ms)\n");
 			}
 		}
 
@@ -605,7 +585,7 @@ static Bool steamPrepare(void)
 
 	if (!(g_realSteamAPI_Init && g_realSteamAPI_InitSafe))
 	{
-		fprintf(stderr, "ERROR: Unable to set up steam hooks. Won't work this way. "
+		log(LOG_ERROR, "Unable to set up steam hooks. Won't work this way. "
 				"Please disable this module from being LD_PRELOAD'ed.\n");
 		return False;
 	}
@@ -625,63 +605,53 @@ static void steamSetup(void)
 
 	if (!sc)
 	{
-		fprintf(stderr, "ERROR: SteamClient is NULL!\n");
+		log(LOG_ERROR, "SteamClient is NULL!\n");
 		return;
 	}
 
 	hsp = SteamAPI_GetHSteamPipe();
 	hsu = SteamAPI_GetHSteamUser();
-#if DEBUG > 1
-	fprintf(stderr, "client: %p\n", sc);
-	fprintf(stderr, "hpipe: %d\n", hsp);
-	fprintf(stderr, "huser: %d\n", hsu);
-#endif
+	log(LOG_INFO, "client=%p hpipe=%d huser=%d\n", sc, hsp, hsu);
 
 	su = sc->vtab->GetISteamUser(sc, hsu, hsp, STEAMUSER_INTERFACE_VERSION);
 	if (!su)
 	{
-		fprintf(stderr, ISTEAMERROR(SteamUser, STEAMUSER_INTERFACE_VERSION));
+		log(LOG_ERROR, ISTEAMERROR(SteamUser, STEAMUSER_INTERFACE_VERSION));
 		return;
 	}
 
 	g_steamUserID = su->vtab->GetSteamID(su);
-#if DEBUG > 1
-	fprintf(stderr, "id: %ju %u\n", g_steamUserID.as64Bit, g_steamUserID.asComponent.accountID);
-	fflush(stderr);
-#endif
+	log(LOG_WARN, "UserID+AccountID: %ju %u\n", g_steamUserID.as64Bit, g_steamUserID.asComponent.accountID);
 
 	sut = sc->vtab->GetISteamUtils(sc, hsp, STEAMUTILS_INTERFACE_VERSION);
 	if (!sut)
 	{
-		fprintf(stderr, ISTEAMERROR(SteamUtils, STEAMUTILS_INTERFACE_VERSION));
+		log(LOG_ERROR, ISTEAMERROR(SteamUtils, STEAMUTILS_INTERFACE_VERSION));
 		return;
 	}
 	g_steamAppID.appId = sut->vtab->GetAppID(sut);
 	g_steamAppID.modId = 0;
 	g_steamAppID.type = 0;
-#if DEBUG > 1
-	fprintf(stderr, "aid: %u\n", g_steamAppID.appId);
-	fflush(stderr);
-#endif
+	log(LOG_WARN, "AppID: %u\n", g_steamAppID.appId);
 
 	g_steamIScreenshot = sc->vtab->GetISteamScreenshots(sc, hsu, hsp, STEAMSCREENSHOTS_INTERFACE_VERSION);
 	if (!g_steamIScreenshot)
 	{
-		fprintf(stderr, ISTEAMERROR(SteamScreenshots, STEAMSCREENSHOTS_INTERFACE_VERSION));
+		log(LOG_ERROR, ISTEAMERROR(SteamScreenshots, STEAMSCREENSHOTS_INTERFACE_VERSION));
 		return;
 	}
 
 	g_steamIUnifiedMessage = sc->vtab->GetISteamUnifiedMessages(sc, hsu, hsp, STEAMUNIFIEDMESSAGES_INTERFACE_VERSION);
 	if (!g_steamIUnifiedMessage)
 	{
-		fprintf(stderr, ISTEAMERROR(SteamUnifiedMessages, STEAMUNIFIEDMESSAGES_INTERFACE_VERSION));
+		log(LOG_ERROR, ISTEAMERROR(SteamUnifiedMessages, STEAMUNIFIEDMESSAGES_INTERFACE_VERSION));
 		return;
 	}
 
 	g_steamIUserStats = sc->vtab->GetISteamUserStats(sc, hsu, hsp, STEAMUSERSTATS_INTERFACE_VERSION);
 	if (!g_steamIUserStats)
 	{
-		fprintf(stderr, ISTEAMERROR(SteamUserStats, STEAMUSERSTATS_INTERFACE_VERSION));
+		log(LOG_ERROR, ISTEAMERROR(SteamUserStats, STEAMUSERSTATS_INTERFACE_VERSION));
 		return;
 	}
 
@@ -768,9 +738,8 @@ extern Display *XOpenDisplay(const char *name)
 {
 	Display *dpy;
 
-#if DEBUG > 2
-	fprintf(stderr, "%s(%s)\n", __FUNCTION__, name);
-#endif
+	log(LOG_DEBUG, "%s(%s)\n", __FUNCTION__, name);
+
 	dpy = (Display *)g_realXOpenDisplay(name);
 
 	if (dpy)
@@ -779,123 +748,84 @@ extern Display *XOpenDisplay(const char *name)
 		 * possibility of dead-locking during runtime for apps doing excessive
 		 * display locking. */
 		g_xKeyCodeF11 = XKeysymToKeycode(dpy, XK_F11);
-#if DEBUG > 2
-		fprintf(stderr, "Handling KeyCode %d as KeySym %d\n", g_xKeyCodeF11, XK_F11);
-#endif
+		log(LOG_NOTICE, "Handling KeyCode %d as KeySym %d\n", g_xKeyCodeF11, XK_F11);
 		g_xKeyCodeF12 = XKeysymToKeycode(dpy, XK_F12);
-#if DEBUG > 2
-		fprintf(stderr, "Handling KeyCode %d as KeySym %d\n", g_xKeyCodeF12, XK_F12);
-#endif
+		log(LOG_NOTICE, "Handling KeyCode %d as KeySym %d\n", g_xKeyCodeF12, XK_F12);
 	}
-#if DEBUG > 2
-	fprintf(stderr, "%s() returning %p\n", __FUNCTION__, dpy);
-#endif
+
+	log(LOG_DEBUG, "%s() returning %p\n", __FUNCTION__, dpy);
 	return dpy;
 }
 
 #if 0
 extern int XGrabKeyboard(Display *dpy, Window win, Bool oe, int pm, int km, Time t)
 {
-#if DEBUG > 2
-	fprintf(stderr, "%s(%p, 0x%lx, %d, 0x%x, 0x%x, 0x%lx)\n", __FUNCTION__, dpy, win, oe, pm, km, t);
-#endif
+	log(LOG_DEBUG, "%s(%p, 0x%lx, %d, 0x%x, 0x%x, 0x%lx)\n", __FUNCTION__, dpy, win, oe, pm, km, t);
 	return GrabSuccess;
 }
 
 extern int XGrabPointer(Display *dpy, Window win, Bool oe, uint32_t em, int pm, int km, Window ct, Cursor c, Time t)
 {
-#if DEBUG > 2
-	fprintf(stderr, "%s(%p, 0x%lx, %d, 0x%x, 0x%x, 0x%x, 0x%lx, 0x%lx, 0x%lx)\n", __FUNCTION__, dpy, win, oe, em, pm, km, ct, c, t);
-#endif
+	log(LOG_DEBUG, "%s(%p, 0x%lx, %d, 0x%x, 0x%x, 0x%x, 0x%lx, 0x%lx, 0x%lx)\n", __FUNCTION__, dpy, win, oe, em, pm, km, ct, c, t);
 	return GrabSuccess;
 }
 
 extern int XUngrabKeyboard(Display *dpy, Time t)
 {
-#if DEBUG > 2
-	fprintf(stderr, "%s(%p, 0x%lx)\n", __FUNCTION__, dpy, t);
-#endif
+	log(LOG_DEBUG, "%s(%p, 0x%lx)\n", __FUNCTION__, dpy, t);
 	return GrabSuccess;
 }
 
 extern int XUngrabPointer(Display *dpy, Time t)
 {
-#if DEBUG > 2
-	fprintf(stderr, "%s(%p, 0x%lx)\n", __FUNCTION__, dpy, t);
-#endif
+	log(LOG_DEBUG, "%s(%p, 0x%lx)\n", __FUNCTION__, dpy, t);
 	return GrabSuccess;
 }
-#endif
 
-#if 0
 extern int XGrabServer(Display *dpy)
 {
-#if DEBUG > 2
-	fprintf(stderr, "%s()\n", __FUNCTION__);
-#endif
+	log(LOG_DEBUG, "%s()\n", __FUNCTION__);
 	return AlreadyGrabbed;
 }
 
 extern int XUngrabServer(Display *dpy)
 {
-#if DEBUG > 2
-	fprintf(stderr, "%s()\n", __FUNCTION__);
-#endif
+	log(LOG_DEBUG, "%s()\n", __FUNCTION__);
 	return 0;
 }
 #endif
 
 extern int XEventsQueued(Display *dpy, int mode)
 {
-#if DEBUG > 3
-	fprintf(stderr, "%s()\n", __FUNCTION__);
-#endif
+	log(LOG_DEBUG, "%s()\n", __FUNCTION__);
 	handleRequest(dpy);
-#if DEBUG > 4
-	fprintf(stderr, "%s() calling real\n", __FUNCTION__);
-#endif
+	log(LOG_DEBUG, "%s() calling real\n", __FUNCTION__);
 	int rc = g_realXEventsQueued(dpy, mode);
-#if DEBUG > 3
-	fprintf(stderr, "%s() returning %d\n", __FUNCTION__, rc);
-#endif
+	log(LOG_DEBUG, "%s() returning %d\n", __FUNCTION__, rc);
 	return rc;
 }
 
-#if 1
 extern int XLookupString(XKeyEvent *ke, char *bufret, int bufsiz,
 		KeySym *keysym, XComposeStatus *status_in_out)
 {
-#if DEBUG > 3
-	fprintf(stderr, "%s()\n", __FUNCTION__);
-#endif
+	log(LOG_DEBUG, "%s()\n", __FUNCTION__);
 	if (filter(ke->display, (XEvent *)ke, NULL))
 	{
 		handleScreenShot(ke->display, ke->window);
 	}
-#if DEBUG > 4
-	fprintf(stderr, "%s() calling real\n", __FUNCTION__);
-#endif
+	log(LOG_DEBUG, "%s() calling real\n", __FUNCTION__);
 	int rc = g_realXLookupString(ke, bufret, bufsiz, keysym, status_in_out);
-#if DEBUG > 3
-	fprintf(stderr, "%s() returning %d\n", __FUNCTION__, rc);
-#endif
+	log(LOG_DEBUG, "%s() returning %d\n", __FUNCTION__, rc);
 	return rc;
 }
-#endif
 
 extern int XPending(Display *dpy)
 {
-#if DEBUG > 3
-	fprintf(stderr, "%s()\n", __FUNCTION__);
-#endif
+	log(LOG_DEBUG, "%s()\n", __FUNCTION__);
 	handleRequest(dpy);
-#if DEBUG > 4
-	fprintf(stderr, "%s() calling real\n", __FUNCTION__);
-#endif
+	log(LOG_DEBUG, "%s() calling real\n", __FUNCTION__);
 	int rc = g_realXPending(dpy);
-#if DEBUG > 3
-	fprintf(stderr, "%s() returning %d\n", __FUNCTION__, rc);
-#endif
+	log(LOG_DEBUG, "%s() returning %d\n", __FUNCTION__, rc);
 	return rc;
 }
 
@@ -903,12 +833,11 @@ extern Bool SteamAPI_Init(void)
 {
 	Bool r;
 
+	log(LOG_DEBUG, "%s()\n", __FUNCTION__);
+
 	if (g_steamInitialized)
 		return True;
 
-#if DEBUG > 1
-	fprintf(stderr, "%s()\n", __FUNCTION__);
-#endif
 	r = steamPrepare();
 
 	if (r)
@@ -924,12 +853,11 @@ extern Bool SteamAPI_InitSafe(void)
 {
 	Bool r;
 
+	log(LOG_DEBUG, "%s()\n", __FUNCTION__);
+
 	if (g_steamInitialized)
 		return True;
 
-#if DEBUG > 1
-	fprintf(stderr, "%s()\n", __FUNCTION__);
-#endif
 	r = steamPrepare();
 
 	if (r)
@@ -944,9 +872,7 @@ extern Bool SteamAPI_InitSafe(void)
 #ifdef _GNU_SOURCE
 extern void *dlsym(void *handle, const char *symbol)
 {
-#if DEBUG > 3
-	fprintf(stderr, "%s(%p, %s)\n", __FUNCTION__, handle, symbol);
-#endif
+	log(LOG_DEBUG, "%s(%p, %s)\n", __FUNCTION__, handle, symbol);
 
 	/* Redirect these symbols through our ones */
 	if (
